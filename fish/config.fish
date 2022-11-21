95a20e8269cb3e71054754552ba3c2a4845a0acd
Petteri Mattila <petterimattila@protonmail.com>
2022-11-21 17:59:03 +0200

    Fish, pipewire, kitty & sway added

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+# SOME GENERAL SETTINGS
+set -U fish_user_paths $fish_user_paths $PATH
+set fish_greeting
+
+# Rust, taken from yangchenyun's config-file
+set -x PATH "$HOME/.cargo/bin" $PATH
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Start X at login
+#if status is-login
+#    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#        exec startx -- -keeptty
+#    end
+#end
+
+# Start Sway - Not necessary
+#if test -z $DISPLAY; and test (tty) = "/dev/tty1"
+#    sway
+#end
---
+++ sway/.bashrc
@@ -0,0 +1,4 @@
+#export WLR_NO_HARDWARE_CURSORS=1
+#if [ "$(tty)" = "/dev/tty1" ]; then
+#	exec dbus-run-session sway
+#fi
