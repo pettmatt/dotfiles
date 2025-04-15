4ca253cf5fb58f5f86aff7fabaa48c3645e64061
Petteri Mattila <petterimattila@protonmail.com>
2025-04-15 12:50:27 +0300

    Nvim default settings + tweaks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Opens file manager
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "C-d", "<C-d>zz")
+vim.keymap.set("n", "C-u", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP") -- Allows pasting over existing word(s) without copying it
+
+-- Controlled usage of clipboards (vim, system, both)
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Switch projects
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionnizer<CR>")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,35 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = " " -- is the character(s) for <leader>
+vim.g.maplocalleader = "\\"
+
+local opt = vim.opt
+
+-- opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.number = true
+opt.relativenumber = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = false -- Use spaces instead of tabs
+opt.scrolloff = 8
+opt.winminwidth = 5 -- Minimum window width
+
+opt.undolevels = 10000
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+if vim.fn.has("nvim-0.10") == 1 then
+	opt.smoothscroll = true
+	opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+	opt.foldmethod = "expr"
+	opt.foldtext = ""
+else
+	opt.foldmethod = "indent"
+	opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
+end
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
