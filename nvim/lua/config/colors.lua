5d1bffb6dab7eb944a398254c6cddb6e9b302954
Petteri Mattila <petterimattila@protonmail.com>
2025-04-15 12:51:34 +0300

    Tweaks

---
+++ nvim/lua/config/colors.lua
@@ -0,0 +1,7 @@
+function MyColors(color)
+	-- color = color or "miasma"
+	-- vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
