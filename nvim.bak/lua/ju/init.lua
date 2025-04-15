ecde292dca5cf2027f2f2143c269fba8f30cabbb
Petteri Mattila <petterimattila@protonmail.com>
2025-04-15 12:52:03 +0300

    backup

---
+++ nvim.bak/lua/ju/init.lua
@@ -0,0 +1,17 @@
+require("ju.keymaps")
+require("ju.lazy_init")
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.scrolloff = 8
+
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+local augroup = vim.api.nvim_create_augroup
+local mainGroup = augroup("ju", {})
---
+++ nvim.bak/lua/ju/keymaps.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " " -- is the character(s) for <leader>
+vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Opens file manager
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "C-d", "<C-d>zz")
+vim.keymap.set("n", "C-u", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP") -- Allows pasting over existing word(s) without copying it
+
+-- Controlled usage of clipboards (vim, system, both)
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Switch projects
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionnizer<CR>")
